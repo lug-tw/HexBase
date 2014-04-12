1928d83cd1be8e833167d8b142beb728052ae6d9
rsghost <im@rsghost.org>
2014-04-12 13:20:57 +0800

    新增街景

--- web/assets/js/logo.js
+++ web/assets/js/logo.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
   var logo = function () {
     var width = $(window).width(),
         height = $(window).height(),
@@ -58,8 +57,61 @@ $(document).ready(function() {
       width  : maxFont() * 2.5,
       height : maxFont() / 2
     });
-
   }
   logo();
+  var map = function () {
+    var initialize = function () {
+      // Set up Street View and initially set it visible. Register the
+      // custom panorama provider function. Set the StreetView to display
+      // the custom panorama 'reception' which we check for below.
+      var panoOptions = {
+        pano: 'reception',
+        zoomControl: false,
+        addressControl: false,
+        linksControl: false,
+        panControl: false,
+        visible: true,
+        panoProvider: getCustomPanorama
+      };
+
+      var panorama = new google.maps.StreetViewPanorama($('#map-canvas')[0], panoOptions);
+    }
+
+    // Return a pano image given the panoID.
+    var getCustomPanoramaTileUrl = function (pano, zoom, tileX, tileY) {
+      // Note: robust custom panorama methods would require tiled pano data.
+      // Here we're just using a single tile, set to the tile size and equal
+      // to the pano "world" size.
+      return 'assets/img/pano4.png';
+    }
+
+    // Construct the appropriate StreetViewPanoramaData given
+    // the passed pano IDs.
+    var getCustomPanorama = function (pano, zoom, tileX, tileY) {
+      if (pano == 'reception') {
+        return {
+          location: {
+            pano: 'reception',
+            description: 'Google Sydney - Reception'
+          },
+          links: [],
+          // The text for the copyright control.
+          copyright: 'Imagery (c) 2010 Google',
+          // The definition of the tiles for this panorama.
+          tiles: {
+            tileSize: new google.maps.Size(1024, 512),
+            worldSize: new google.maps.Size(1024, 512),
+            // The heading in degrees at the origin of the panorama
+            // tile set.
+            centerHeading: 105,
+            getTileUrl: getCustomPanoramaTileUrl
+          }
+        };
+      }
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+  }
+  map();
   $(window).resize(logo);
 });
